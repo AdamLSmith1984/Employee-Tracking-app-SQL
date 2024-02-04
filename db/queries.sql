c9c75065281f980fd15250e97ef9f972b1add1cc
Adam Smith <asmith5144@hotmail.com>
2024-02-04 13:56:37 -0500

    edited queries.sql

---
+++ db/queries.sql
@@ -0,0 +1,10 @@
+SELECT department.department_name, department.id AS department_id
+FROM department;
+SELECT role.title AS job_title, role.id AS role_id, department.department_name AS department, role.salary
+FROM role
+JOIN department ON role.department_id = department_id;
+SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, department.department_name AS department, role.salary, manager.first_name AS manager_first_name, manager_last_name AS manager_last_name
+FROM employee
+JOIN role ON employee.role_id = role_id
+JOIN department ON role.department_id = department_id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
